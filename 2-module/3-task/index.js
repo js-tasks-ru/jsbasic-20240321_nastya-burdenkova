f5cd796618b7e7135033f17e726b831d5afeb8c9
kamille57 <126716411+kamille57@users.noreply.github.com>
2024-04-03 22:59:08 +0300

    add solution for calculator

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,21 @@
 let calculator = {
-  // ваш код
+  a: null,
+  b: null,
+  read: function (a, b) {
+    this.a = a;
+    this.b = b;
+  },
+  sum: function () {
+    return this.a + this.b;
+  },
+  mul: function () {
+    return this.a * this.b;
+  },
 };
+
+calculator.read(3, 5);
+console.log(calculator.sum()); // 8
+console.log(calculator.mul()); // 15
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
