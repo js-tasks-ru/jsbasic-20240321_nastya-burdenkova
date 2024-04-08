706573b858cc49f858c8f17fea52d5fc68ad241a
kamille57 <126716411+kamille57@users.noreply.github.com>
2024-04-08 19:28:40 +0300

    add solution for table

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,29 @@
 function highlight(table) {
-  // ваш код...
+  const rows = table.querySelectorAll('tbody tr');
+
+  rows.forEach(row => {
+    const status = row.querySelector('td[data-available]');
+    const gender = row.querySelector('td:nth-child(3)');
+    const age = row.querySelector('td:nth-child(2)');
+
+    if (status) {
+      if (status.dataset.available === "true") {
+        row.classList.add('available');
+      } else if (status.dataset.available === "false") {
+        row.classList.add('unavailable');
+      }
+    } else {
+      row.setAttribute('hidden', true);
+    }
+
+    if (gender.textContent === "m") {
+      row.classList.add('male');
+    } else if (gender.textContent === "f") {
+      row.classList.add('female');
+    }
+
+    if (parseInt(age.textContent) < 18) {
+      row.style.textDecoration = 'line-through';
+    }
+  });
 }
