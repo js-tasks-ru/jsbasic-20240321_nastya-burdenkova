cc7290cfb3830486de788ce1014f85580b46bdb3
kamille57 <126716411+kamille57@users.noreply.github.com>
2024-04-20 23:14:33 +0300

    add basket features

--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -1,4 +1,37 @@
-export default class ProductCard {
+import createElement from "../../assets/lib/create-element.js";
+
+class ProductCard {
   constructor(product) {
+    this.product = product;
+
+    this.render();
+    this.elem.addEventListener("click", this.onButtonClick);
   }
+
+  render() {
+    this.elem = createElement(`
+            <div class="card">
+                <div class="card__top">
+                    <img src="/assets/images/products/${this.product.image}" class="card__image" alt="product">
+                    <span class="card__price">€${this.product.price.toFixed(2)}</span>
+                </div>
+                <div class="card__body">
+                    <div class="card__title">${this.product.name}</div>
+                    <button type="button" class="card__button">
+                        <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+                    </button>
+                </div>
+            </div>
+        `);
+  }
+
+  onButtonClick = () => {
+    let event = new CustomEvent("product-add", {
+      detail: this.product.id,
+      bubbles: true,
+    });
+    this.elem.dispatchEvent(event);
+  };
 }
+
+export default ProductCard;
